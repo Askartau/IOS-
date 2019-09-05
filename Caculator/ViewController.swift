b188c3a1421d11d3148b741b9092e9c9ca375041
Askar <Ask4r.pr@gmail.com>
2019-09-05 16:23:40 +0600

    Initial Commit

---
+++ Caculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Caculator
+//
+//  Created by Askar Serikkhanov on 9/5/19.
+//  Copyright © 2019 Askar Serikkhanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
